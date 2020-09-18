5127dffd41a6bc5aedc48404f8b36e25c51f3e79
Marcin Wasiluk <marcin@wasiluk.com>
2020-09-18 23:20:31 +0100

    Init

---
+++ src/main/java/com/sample/demo/memoryleakingapp/MemoryLeakingAppApplication.java
@@ -0,0 +1,13 @@
+package com.sample.demo.memoryleakingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryLeakingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryLeakingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/demo/memoryleakingapp/MemoryLeakingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.demo.memoryleakingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryLeakingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
